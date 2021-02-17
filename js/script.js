69289cc3bf574c724c0c06122f4772325f49001b
Dima <example@gmail.com>
2021-02-17 21:04:40 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert('Hello');
+
+//const result = confirm("Are u here?");
+
+// const answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('Как Ваше имя?', '');
+answers[1] = prompt('Как Ваша фамилия?', '');
+answers[2] = prompt('Сколько Вам лет?', '');
+
+document.write(answers);
+
